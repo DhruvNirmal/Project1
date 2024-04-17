ec3b7a0f0ffd4c90d0b0b936f360d49150cb25ab
DhruvNirmal <dnir0002@student.monash.edu>
2024-04-17 20:28:29 +1000

    guarantor and underwriter

--- Initial_EDA.sql
+++ Initial_EDA.sql
@@ -1,5 +1,11 @@
 USE Project1;
 
+
+
+
+Select * 
+from California;
+
 SELECT purpose, Final_Maturity_Date, Sale_date
 FROM California
 where purpose = 'Health Care Facilities';
@@ -22,8 +28,7 @@ ORDER BY number_of_bonds_by_issuer DESC;
 SELECT Purpose,
 COUNT(*) AS number_of_bonds_by_purpose,
 SUM(Principal_Amount) total_amount,
-AVG(Principal_Amount) AS Average_amount_per_bond,
-AVG(DATEDIFF(day, Final_Maturity_Date, Sale_date)) as Average_bond_length
+AVG(Principal_Amount) AS Average_amount_per_bond
 FROM California
 GROUP BY Purpose
 ORDER BY number_of_bonds_by_purpose DESC;
@@ -49,8 +54,9 @@ SELECT
 purpose,
 year(Sale_Date) as Years,
 SUM(Principal_Amount) Total,
-SUM(COALESCE([Total_Issuance_Costs],0) + COALESCE([UW_Takedown],0) + COALESCE([UW_Mngmt_Fee],0) + COALESCE([UW_Expenses],0) + COALESCE([UW_Total_Discount_Spread],0) + COALESCE([Placement_Agent_Fee],0) + COALESCE([Financial_Advisor_Fee],0) + COALESCE([Bond_Counsel_Fee],0) + COALESCE([Co_Bond_Counsel_Fee],0) + COALESCE([Disclosure_Counsel_Fee],0) + COALESCE([Borrower_Counsel_Fee],0) + COALESCE([Trustee_Fee],0) + COALESCE([Credit_Enhancement_Fee],0) + COALESCE([Rating_Agency_Fee],0) + COALESCE([Other_Issuance_Expenses],0)) as Average_Expenses,
-AVG(TIC_Interest_Rate) as Average_interest_rate
+AVG(COALESCE([Total_Issuance_Costs],0) + COALESCE([UW_Takedown],0) + COALESCE([UW_Mngmt_Fee],0) + COALESCE([UW_Expenses],0) + COALESCE([UW_Total_Discount_Spread],0) + COALESCE([Placement_Agent_Fee],0) + COALESCE([Financial_Advisor_Fee],0) + COALESCE([Bond_Counsel_Fee],0) + COALESCE([Co_Bond_Counsel_Fee],0) + COALESCE([Disclosure_Counsel_Fee],0) + COALESCE([Borrower_Counsel_Fee],0) + COALESCE([Trustee_Fee],0) + COALESCE([Credit_Enhancement_Fee],0) + COALESCE([Rating_Agency_Fee],0) + COALESCE([Other_Issuance_Expenses],0)) as Average_Expenses,
+AVG(TIC_Interest_Rate) as Average_interest_rate,
+AVG(DATEDIFF(day, Sale_date, Final_Maturity_Date)) as Average_bond_length --i only want to use values where maturity date is not 1900-01-01 but only for this column
 from California
 WHERE Principal_Amount <> 0
 GROUP BY year(Sale_Date), purpose
@@ -59,9 +65,65 @@ Order BY Years;
 -- interest rate 
 
 
-SELECT AVG(TIC_Interest_Rate) as Average_interest_rate,
-year(Sale_date) as Years
+SELECT year(Sale_date) as Years,
+AVG(CASE WHEN Interest_Type = 'TIC' THEN TIC_Interest_Rate END) as Average_interest_rate_TIC,
+AVG(CASE WHEN Interest_Type = 'NIC' THEN NIC_Interest_Rate END) as Average_interest_rate_NIC
 From California
 GROUP BY year(Sale_date)
 ORDER BY year(Sale_date);
 
+-- Average bond time length by purpose
+
+SELECT 
+purpose,
+year(Sale_date) as years,
+AVG(DATEDIFF(day, Sale_date, Final_Maturity_Date)) as Average_bond_length
+from California
+where Final_Maturity_Date <> '1900-01-01'
+GROUP BY purpose, year(Sale_date)
+order by years;
+
+-- Issue sale type comp/neg   (gotta split tables by NIC and TIC)
+
+Select [Sale_Type_Comp_Neg], 
+year(Sale_date) as years,
+AVG([TIC_Interest_Rate]) as Average_interest_rate,
+AVG(COALESCE([Total_Issuance_Costs],0) + COALESCE([UW_Takedown],0) + COALESCE([UW_Mngmt_Fee],0) + COALESCE([UW_Expenses],0) + COALESCE([UW_Total_Discount_Spread],0) + COALESCE([Placement_Agent_Fee],0) + COALESCE([Financial_Advisor_Fee],0) + COALESCE([Bond_Counsel_Fee],0) + COALESCE([Co_Bond_Counsel_Fee],0) + COALESCE([Disclosure_Counsel_Fee],0) + COALESCE([Borrower_Counsel_Fee],0) + COALESCE([Trustee_Fee],0) + COALESCE([Credit_Enhancement_Fee],0) + COALESCE([Rating_Agency_Fee],0) + COALESCE([Other_Issuance_Expenses],0)) as Average_Expenses
+from California
+GROUP BY [Sale_Type_Comp_Neg], year(Sale_date)
+ORDER BY years;
+
+-- Most famous underwriter (do this analysis with guarantor as well) 
+
+SELECT Underwriter, 
+COUNT(Underwriter) as Number_of_bonds,
+COUNT(case when Moody_Rating LIKE '%M:Aa%' OR Moody_Rating LIKE 'M:P-1' THEN 1 END) AS good_bonds_by_moody,
+COUNT(case when S_and_P_Rating LIKE '%AA%' OR S_and_P_Rating LIKE 'S:A-1%' THEN 1 END) AS good_bonds_by_moody,
+AVG(Principal_Amount) AS Average_principal_amount
+from California
+GROUP BY Underwriter
+ORDER BY Number_of_bonds DESC;
+
+-- Most famous guarantor 
+
+SELECT Guarantor, 
+COUNT(Guarantor) as Number_of_bonds,
+COUNT(case when Moody_Rating LIKE '%Aa%' OR Moody_Rating LIKE 'M:P-1' THEN 1 END) AS good_bonds_by_moody,
+COUNT(case when S_and_P_Rating LIKE '%AA%' OR S_and_P_Rating LIKE 'S:A-1%' THEN 1 END) AS good_bonds_by_SandP,
+AVG(Principal_Amount) AS Average_principal_amount 
+from California
+GROUP BY Guarantor
+ORDER BY Number_of_bonds DESC;
+
+
+-- Checking something
+SELECT *
+FROM California
+where Guarantor in ('Comerica Bank');
+
+Select Moody_Rating, count(*) as x
+from California
+GROUP BY Moody_Rating
+order by x DESC;
+
+-- 
